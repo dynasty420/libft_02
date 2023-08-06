c4fe4c6a26fdd6cc05657e3d6a9e033d263b5786
Yutaro Torigoe <ytorigoe@c1r14s6.42tokyo.jp>
2023-08-06 20:37:42 +0900

    a

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yut <yut@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: ytorigoe <ytorigoe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 15:12:19 by yut               #+#    #+#             */
-/*   Updated: 2023/08/06 15:29:22 by yut              ###   ########.fr       */
+/*   Updated: 2023/08/06 20:37:15 by ytorigoe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,21 @@
 char	*ft_strdup(const char *s1)
 {
 	char	*dest;
-	size_t	i;
+	size_t	len;
 
-	dest = (char *)malloc(ft_strlen(s1) + 1);
+	len = ft_strlen(s1);
+
+	dest = (char *)malloc(len + 1);
 	if (dest == NULL)
 		return (NULL);
-	i = 0;
-	while (s1[i] != '\0')
-	{
-		dest[i] = s1[i];
-		i++;
-	}
-	dest[i] = '\0';
+	ft_strlcpy(dest, (char *)s1, len);
 	return (dest);
 }
 
-// size_t	ft_strlen(const char *s)
-// {
-// 	size_t	len;
-
-// 	len = 0;
-// 	while (s[len] != '\0')
-// 		len++;
-// 	return (len);
-// }
-
 // int main()
 // {
-// 	char *ptr = ft_strdup("hello");
-// 	printf("%s\n", ptr);
-// 	printf("%p\n", ptr);
-// 	printf("%lu\n", sizeof(ptr));
+// 	char *ptr1 = ft_strdup("hello");
+// 	char *ptr2 = strdup("hello");
+	
+// 	printf("%d\n",(ptr1 == ptr2));
 // }
